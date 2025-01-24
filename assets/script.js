f9517faee996c72c310ead470fe7018cdf0c5d1f
popov-da <popov-da@users.noreply.github.com>
2025-01-24 10:35:04 +0000

    deploy: 52fd98613f3054bf1db866d0bef89e71cf0eecb8

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,95 @@
-function hideStartPage(){document.getElementById("startPage").style.display="none"}function showStartPage(){document.getElementById("loadingIndicator").style.display="none",document.getElementById("contentFrame").src="about:blank",document.getElementById("startPage").style.display="flex",document.getElementById("exploitationSection").style.display="block",document.getElementById("EducateSection").style.display="block",document.getElementById("technicalSection").style.display="block",document.getElementById("IETPSection").style.display="block",document.getElementById("PresSection").style.display="block"}function loadContent(e,t){hideStartPage();const n=document.getElementById("loadingIndicator");n.style.display="flex",document.querySelectorAll(".file-item").forEach((e=>e.classList.remove("active"))),e.classList.add("active");const l=document.getElementById("contentFrame");l.onload=function(){n.style.display="none"},l.src=t}function loadPdf(e,t){hideStartPage(),e.stopPropagation(),loadContent(e.target.closest(".file-item"),t)}function toggleStructure(){const e=document.getElementById("structure"),t=document.getElementById("contentArea"),n=document.getElementById("toggleIcon");e.classList.toggle("hidden"),t.classList.toggle("full-width"),n.textContent=e.classList.contains("hidden")?"▶":"◀"}function showExploitationDocs(){hideStartPage(),document.getElementById("selectMessage").style.display="flex",document.getElementById("exploitationSection").style.display="block",document.getElementById("EducateSection").style.display="none",document.getElementById("technicalSection").style.display="none",document.getElementById("IETPSection").style.display="none",document.getElementById("PresSection").style.display="none"}function showTechnicalDocs(){hideStartPage(),document.getElementById("selectMessage").style.display="flex",document.getElementById("exploitationSection").style.display="none",document.getElementById("EducateSection").style.display="none",document.getElementById("technicalSection").style.display="block",document.getElementById("IETPSection").style.display="none",document.getElementById("PresSection").style.display="none"}function showIETPDocs(){hideStartPage(),document.getElementById("selectMessage").style.display="flex",document.getElementById("exploitationSection").style.display="none",document.getElementById("EducateSection").style.display="block",document.getElementById("technicalSection").style.display="none",document.getElementById("IETPSection").style.display="block",document.getElementById("PresSection").style.display="none"}function loadContent(e,t){hideStartPage(),document.getElementById("selectMessage").style.display="none";const n=document.getElementById("loadingIndicator");n.style.display="flex",document.querySelectorAll(".file-item").forEach((e=>e.classList.remove("active"))),e.classList.add("active");const l=document.getElementById("contentFrame");l.onload=function(){n.style.display="none"},l.src=t}
+    // Функция скрытия стартовой страницы
+    function hideStartPage() {
+      const startPage = document.getElementById('startPage');
+      startPage.style.display = 'none';
+    }
+    // Функция отображения стартовой страницы
+    function showStartPage() {
+      // Скрываем индикатор загрузки и останавливаем загрузку фрейма
+      document.getElementById('loadingIndicator').style.display = 'none';
+      document.getElementById('contentFrame').src = 'about:blank';
+      // Отображаем стартовую страницу и обе секции
+      document.getElementById('startPage').style.display = 'flex';
+      document.getElementById('exploitationSection').style.display = 'block';
+      document.getElementById('EducateSection').style.display = 'block';
+      document.getElementById('technicalSection').style.display = 'block';
+      document.getElementById('IETPSection').style.display = 'block';
+      document.getElementById('PresSection').style.display = 'block';
+    }
+    // Функция загрузки HTML-контента или PDF в iframe с подсветкой активной вкладки
+    function loadContent(element, pageUrl) {
+      hideStartPage(); // Скрыть стартовую страницу при загрузке любого элемента
+      const loadingIndicator = document.getElementById('loadingIndicator');
+      loadingIndicator.style.display = 'flex';
+      // Снятие активного класса с предыдущей вкладки
+      document.querySelectorAll('.file-item').forEach(item => item.classList.remove('active'));
+      // Установка активного класса на текущую вкладку
+      element.classList.add('active');
+      const iframe = document.getElementById('contentFrame');
+      iframe.onload = function () {
+        loadingIndicator.style.display = 'none';
+      };
+      iframe.src = pageUrl;
+    }
+    // Функция загрузки PDF и предотвращения всплытия
+    function loadPdf(event, pdfUrl) {
+      hideStartPage(); // Скрыть стартовую страницу при загрузке любого PDF
+      event.stopPropagation();
+      loadContent(event.target.closest('.file-item'), pdfUrl);
+    }
+    // Функция для скрытия/показа структуры
+    function toggleStructure() {
+      const structure = document.getElementById('structure');
+      const contentArea = document.getElementById('contentArea');
+      const toggleIcon = document.getElementById('toggleIcon');
+      structure.classList.toggle('hidden');
+      contentArea.classList.toggle('full-width');
+      toggleIcon.textContent = structure.classList.contains('hidden') ? '▶' : '◀';
+    }
+
+    function showExploitationDocs() {
+      hideStartPage();
+      document.getElementById('selectMessage').style.display = 'flex';
+      // Показ только раздела "Большая траверса"
+      document.getElementById('exploitationSection').style.display = 'block';
+      document.getElementById('EducateSection').style.display = 'none';
+      document.getElementById('technicalSection').style.display = 'none';
+      document.getElementById('IETPSection').style.display = 'none';
+      document.getElementById('PresSection').style.display = 'none';
+    }
+
+    function showTechnicalDocs() {
+      hideStartPage();
+      document.getElementById('selectMessage').style.display = 'flex';
+      // Показ только раздела "Модуль"
+      document.getElementById('exploitationSection').style.display = 'none';
+      document.getElementById('EducateSection').style.display = 'none';
+      document.getElementById('technicalSection').style.display = 'block';
+      document.getElementById('IETPSection').style.display = 'none';
+      document.getElementById('PresSection').style.display = 'none';
+    }
+
+    function showIETPDocs() {
+      hideStartPage();
+      document.getElementById('selectMessage').style.display = 'flex';
+      // Показ только раздела "Линейная траверса"
+      document.getElementById('exploitationSection').style.display = 'none';
+      document.getElementById('EducateSection').style.display = 'block';
+      document.getElementById('technicalSection').style.display = 'none';
+      document.getElementById('IETPSection').style.display = 'block';
+      document.getElementById('PresSection').style.display = 'none';
+    }
+
+    function loadContent(element, pageUrl) {
+      hideStartPage();
+      document.getElementById('selectMessage').style.display = 'none'; // Скрываем сообщение
+      const loadingIndicator = document.getElementById('loadingIndicator');
+      loadingIndicator.style.display = 'flex';
+      document.querySelectorAll('.file-item').forEach(item => item.classList.remove('active'));
+      element.classList.add('active');
+      const iframe = document.getElementById('contentFrame');
+      iframe.onload = function () {
+        loadingIndicator.style.display = 'none';
+      };
+      iframe.src = pageUrl;
+    }
--- files/C3D/res/structure.js
+++ files/C3D/res/structure.js
@@ -1,1 +1,58 @@
-!function(e){e.uniview||(e.uniview={config:{}}),e.uniview.config||(e.uniview.config={});var o=e.uniview.config;o.soloUrl&&(e.baseURL=o.soloUrl);var n=o.totalMemory||o.mem||16,r=o.src,t=o.skinUrl||"",a=o.specUrl||"";if(location.search&&(/[\?&]mem=(\d+)/.exec(location.search)&&(n=Math.min(Math.max(16,parseInt(RegExp.$1)),1024)),"?"!=location.search)){var i=location.search.substring(1).split("&")[0];i.indexOf("=")<0&&(r=i)}e.use("require").then((function(){return e.use("Cortona3DSoloSkin",{baseUrl:t})})).then((function(){return e.skin.create("app").use("solo-structure",{features:+o.features||0,baseUrl:a,src:r,totalMemory:n})})).catch((function(e){"object"==typeof e&&"string"==typeof e.message&&/(Cannot enlarge memory)/i.test(e.message)&&n<1024&&(n<256?n*=2:n<1024&&(n+=128),location.replace(location.origin+location.pathname+(location.search||"?").replace(/&mem=\d+/,"")+"&mem="+n)),console.error(e)}))}(Cortona3DSolo);
+(function (solo) {
+    if (!solo.uniview) solo.uniview = {
+        config: {}
+    };
+    if (!solo.uniview.config) solo.uniview.config = {};
+
+    var config = solo.uniview.config;
+
+    if (config.soloUrl) solo.baseURL = config.soloUrl;
+
+    var m_mem = config.totalMemory || config.mem || 16,
+        m_src = config.src,
+        m_skinUrl = config.skinUrl || '',
+        m_specUrl = config.specUrl || '';
+
+    if (location.search) {
+        if (/[\?&]mem=(\d+)/.exec(location.search)) {
+            m_mem = Math.min(Math.max(16, parseInt(RegExp.$1)), 1024);
+        }
+        if (location.search != '?') {
+            var p = location.search.substring(1).split('&')[0];
+            if (p.indexOf('=') < 0) {
+                m_src = p;
+            }
+        }
+    }
+
+    solo.use('require')
+        .then(function () {
+            return solo.use("Cortona3DSoloSkin", {
+                baseUrl: m_skinUrl
+            });
+        })
+        .then(function () {
+            // load solo-uniview as app skin
+            return solo.skin.create('app').use('solo-structure', {
+                features: +config.features || 0,
+                baseUrl: m_specUrl,
+                src: m_src,
+                totalMemory: m_mem
+            });
+        })
+        .catch(function (e) {
+            if (typeof e === 'object' && typeof e.message === 'string') {
+                if (/(Cannot enlarge memory)/i.test(e.message)) {
+                    if (m_mem < 1024) {
+                        if (m_mem < 256) {
+                            m_mem *= 2;
+                        } else if (m_mem < 1024) {
+                            m_mem += 128;
+                        }
+                        location.replace(location.origin + location.pathname + (location.search || '?').replace(/&mem=\d+/, '') + '&mem=' + m_mem);
+                    }
+                }
+            }
+            console.error(e);
+        });
+})(Cortona3DSolo);
